1b043be6f9d88eac96a9a5a95f928385002f783b
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-16 15:51:21 -0500

    menu issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const menuDrop = document.querySelector("#dropmenu");
 document.addEventListener("mousedown", function (i) {
   if (i.target === btn || i.target.closest(".burger")) {
     menuDrop.classList.toggle("active");
-  } else if (i.target === menuDrop) return;
+  } else if (i.target !== menuDrop) return;
   else if (
     !i.target.matches("#burger_container") ||
     !i.target.closest(".burger")
