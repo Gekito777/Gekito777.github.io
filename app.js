41fa389c6488530e467fa0ddf19b6e1f26d22025
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-16 21:15:00 -0500

    needed to fiddle with the drop menu

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const btn = document.querySelector("#burger_container");
 const menuDrop = document.querySelector("#dropmenu");
-
+const aTag = document.querySelectorAll("a");
 // btn.addEventListener("click", function (i) {});
 
 document.addEventListener("mousedown", function (i) {
   if (i.target === btn || i.target.closest(".burger")) {
     menuDrop.classList.toggle("active");
-  } else if (i.target !== menuDrop) return;
-  else if (
-    !i.target.matches("#burger_container") ||
-    !i.target.closest(".burger")
-  ) {
+  } else if (i.target.matches("#menu_container") || !i.target.parentNode) {
     menuDrop.classList.remove("active");
+    console.log(i.target.parentNode);
   }
 });
+
+// window.addEventListener("mouseup", function (e) {
+//   if (e.target != menuDrop && e.target.parentNode != menuDrop) {
+//     console.log(e);
+//   }
+// });
 
 // document.body.addEventListener(`click`, function (e) {
 //   console.log(e.target.closest);
