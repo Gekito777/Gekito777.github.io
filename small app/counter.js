9eeaa43d5d0b59bc727cede8565e32aba83393b2
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-18 06:25:41 -0500

    counter
    needed a few fixes still working on it

--- small app/counter.js
+++ small app/counter.js
@@ -1,15 +1,15 @@
 const countermeter = document.querySelector("#counter");
 const counterInput = document.querySelector("#newText");
 let counter = 0;
-let maxCount = 31;
+let maxCount = 30;
 
 counterInput.addEventListener('keydown', function(e){
 
   if ( counterInput.value.length !== maxCount) {   
 
-
+    let counter = counterInput.value.length
     
-    countermeter.innerText = `${counterInput.value.length}/30`;
+    countermeter.innerText = `${counter+1} /30`;
     let newcount = parseInt(countermeter.innerText)
     console.log(e)}
 
