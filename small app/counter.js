0d3d853b03a253f7ebe3b71af9e6be4053f34abb
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-18 06:11:19 -0500

    word counter completed
    counter logic was changed, will work on main page now

--- small app/counter.js
+++ small app/counter.js
@@ -1,7 +1,7 @@
 const countermeter = document.querySelector("#counter");
 const counterInput = document.querySelector("#newText");
 let counter = 0;
-let maxCount = 30;
+let maxCount = 31;
 // counterInput.addEventListener("input", function (j) {
 // if(counterInput.value !== "" || counter < 30)
 // { counter++
@@ -13,17 +13,13 @@ let maxCount = 30;
 // });
 counterInput.addEventListener('keydown', function(e){
 
-  if (counterInput.value !== "" || counter === 30) {   
+  if ( counterInput.value.length !== maxCount) {   
 
-    counter++
-    countermeter.innerText = `${counter}/30`;
+
+    
+    countermeter.innerText = `${counterInput.value.length}/30`;
     let newcount = parseInt(countermeter.innerText)
     console.log(e)}
 
-  if(e.key !== "Backspace" || e.key !== "Delete"){ 
-   
-    
-  }
-
-  else return;
+  
 });
