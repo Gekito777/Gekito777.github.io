fec557d9f96646304ae53c64d1797986ad8d670c
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-18 06:13:42 -0500

    cleaning up

--- small app/counter.js
+++ small app/counter.js
@@ -2,15 +2,7 @@ const countermeter = document.querySelector("#counter");
 const counterInput = document.querySelector("#newText");
 let counter = 0;
 let maxCount = 31;
-// counterInput.addEventListener("input", function (j) {
-// if(counterInput.value !== "" || counter < 30)
-// { counter++
-//   countermeter.innerText = `${counter}/30`;
-//   // console.log(countermeter.innerText.length)
-//   console.log(j)
-// };
-//   return counter;
-// });
+
 counterInput.addEventListener('keydown', function(e){
 
   if ( counterInput.value.length !== maxCount) {   
