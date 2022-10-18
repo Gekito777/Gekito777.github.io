6f1e6885cc0b90013ddccab03ffd94225a7971fa
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-17 19:16:10 -0500

    test

--- small app/counter.js
+++ small app/counter.js
@@ -1,10 +1,29 @@
 const countermeter = document.querySelector("#counter");
 const counterInput = document.querySelector("#newText");
 let counter = 0;
-counterInput.addEventListener("input", function (j) {
-  if (counterInput.nodeValue !== "") {
-    counter++;
-    countermeter.textContent = counter;
-    console.log(j);
-  } else if (j.inpuType === "deleteContentBackward") counter--;
+let maxCount = 30;
+// counterInput.addEventListener("input", function (j) {
+// if(counterInput.value !== "" || counter < 30)
+// { counter++
+//   countermeter.innerText = `${counter}/30`;
+//   // console.log(countermeter.innerText.length)
+//   console.log(j)
+// };
+//   return counter;
+// });
+counterInput.addEventListener('keydown', function(e){
+
+  if (counterInput.value !== "" || counter === 30) {   
+
+    counter++
+    countermeter.innerText = `${counter}/30`;
+    let newcount = parseInt(countermeter.innerText)
+    console.log(e)}
+
+  if(e.key !== "Backspace" || e.key !== "Delete"){ 
+   
+    
+  }
+
+  else return;
 });
