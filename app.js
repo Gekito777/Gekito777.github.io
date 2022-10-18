54f6ee285a9dd808397352d12e72d476ab94979e
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-10-18 12:05:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const btn = document.querySelector("#burger_container");
 const menuDrop = document.querySelector("#dropmenu");
 const aTag = document.querySelectorAll("a");
-// btn.addEventListener("click", function (i) {});
 
 document.addEventListener("mousedown", function (i) {
-  if (i.target === btn || i.target.closest(".burger")) {
+
+  if (i.target === btn || i.target.closest(".burger")){
     menuDrop.classList.toggle("active");
   } else if (i.target.matches("#menu_container") || !i.target.parentNode) {
     menuDrop.classList.remove("active");
@@ -12,12 +12,3 @@ document.addEventListener("mousedown", function (i) {
   }
 });
 
-// window.addEventListener("mouseup", function (e) {
-//   if (e.target != menuDrop && e.target.parentNode != menuDrop) {
-//     console.log(e);
-//   }
-// });
-
-// document.body.addEventListener(`click`, function (e) {
-//   console.log(e.target.closest);
-// });
