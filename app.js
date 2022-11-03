660921a5dbebfee1b981e43349f6840c672daf93
Gekito777 <72028222+Gekito777@users.noreply.github.com>
2022-11-02 21:16:25 -0500

    tweeks
    menu btn

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ document.addEventListener("mousedown", function (i) {
 
   if (i.target === btn || i.target.closest(".burger")){
     menuDrop.classList.toggle("active");
+    btn.classList.toggle("active")
+    
   } else if (i.target.matches("#menu_container") || !i.target.parentNode) {
     menuDrop.classList.remove("active");
-    console.log(i.target.parentNode);
+    btn.classList.remove("active");
+    
   }
-});
+  
+  });
 
